af3e600aa3b8a89a3df6860e6d1af2b99dd887ad
alekseyratkevich <alekseyratkevich2001@gmail.com>
2021-11-02 14:04:35 +0300

    feat: complete 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const fileName = path.join(__dirname, 'text.txt')
+const output = fs.createWriteStream(fileName)
+const { stdin, stdout } = process
+
+stdout.write('\n--- Пожалуйста, введите ваш текст:\n')
+let resText = ''
+stdin.on('data', data => {
+    let curText = data.toString()
+    if (curText.toUpperCase() === 'EXIT\r\n') {
+        process.exit()
+    } else {
+        resText += curText
+        output.write(curText)
+    }
+})
+
+process.on('SIGINT', () => {
+    process.exit()
+})
+process.on('exit', () => {
+    stdout.write('\n--- Файл сохранён.\r\nДо свидания!\r\n')
+})
